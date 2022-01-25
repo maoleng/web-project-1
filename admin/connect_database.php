71e5a253f514004ff16764823ad59c528345f2a3
Bùi Hữu Lộc <feature451@gmail.com>
2022-01-25 15:09:53 +0700

    format and fix the validation

--- admin/connect_database.php
+++ admin/connect_database.php
@@ -1,6 +1,6 @@
 <?php 
 
-$connect_database = mysqli_connect('localhost', 'root', '', '31');
+$connect_database = mysqli_connect('localhost', 'feature45', '', 'quan_ly_ban_hang');
 mysqli_set_charset($connect_database, 'utf8');
 
 
