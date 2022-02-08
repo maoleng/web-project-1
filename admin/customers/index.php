06ba1e880d3a489fb883236ef8a1c544d4c6a6db
Bùi Hữu Lộc <feature451@gmail.com>
2022-02-08 12:55:02 +0700

    format customer manage

--- admin/customers/index.php
+++ admin/customers/index.php
@@ -5,10 +5,11 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" href="../index1.css">
+	<link rel="stylesheet" href="../index3.css">
 	<link rel="stylesheet" href="../style_validate1.css">
+	<link rel="stylesheet" type="text/css" href="../style_table.css">
 </head>
-<body>
+
 
 <?php require '../connect_database.php';
 if ( empty($_GET['index_page']) ) {
@@ -30,7 +31,7 @@ $customers_skipped = ( $index_page - 1) * $customers_per_page;
 
 
 $sql_select_customers = "
-	SELECT customers.id as 'id', customers.name as 'name', customers.address as 'address', IFNULL(sum(receipts.total_price),0) as 'money', IFNULL(MAX(receipts.order_time), 'Chưa mua lần nào') as 'last_time'
+	SELECT customers.id as 'id', customers.name as 'name', IFNULL(sum(receipts.total),0) as 'money', IFNULL(MAX(receipts.order_time), 'Chưa mua lần nào') as 'last_time'
 	FROM receipts
 	RIGHT JOIN customers ON receipts.customer_id = customers.id
 	GROUP BY customers.id
@@ -40,71 +41,56 @@ $sql_select_customers = "
 $query_sql_select_customers = mysqli_query($connect_database, $sql_select_customers);
 
 ?>
-
-<div class="all">
-	<div class="left">
-		<?php require '../menu.php'; ?>
-	</div> 
-
-
-	<div class="right">
-		<div class="top">
-
-			<div class = "search">
-				<form class = "form_search">
-					Tìm kiếm
-					<input type="search" name="search" value = "<?php echo $content_search ?>">
-					<button>
-						<img src="../style/style_image/icon_search.png" width="50px">
-					</button>
-				</form>
-			</div>
-
-			<div class = "login">
-				<a class = "login" href="https://google.com">Đăng nhập</a>
-			</div> 
-		</div>
-
-
-		<div class = "bot">
-
-			<div class = "header">
-				<h1 class = "header" >KHÁCH HÀNG</h1>
-			</div>
-			<br>
-			<?php require '../validate.php' ?>
-			<table class = "table">
-				<tr>
-					<th>Mã</th>
-					<th>Tên khách hàng</th>
-					<th>Địa chỉ</th>
-					<th>Lần cuối mua hàng</th>
-					<th>Số tiền bỏ ra</th>
-					<th>Xem chi tiết</th>
-				</tr>
-				<?php foreach ($query_sql_select_customers as $each_customer) { ?>
-				<tr>
-					<td><?php echo $each_customer['id'] ?></td>
-					<td><?php echo $each_customer['name'] ?></td>
-					<td><?php echo $each_customer['address'] ?></td>
-					<td><?php echo $each_customer['last_time'] ?></td>
-					<td><?php echo $each_customer['money'] ?></td>
-					<td>
-						<a href="detail_customer.php?id=<?php echo $each_customer['id'] ?>">Xem</a>
-					</td>
-				</tr>
-				<?php } ?>
-			</table>
-			<?php for ( $i = 1; $i <= $pages; $i++) {  ?>
-				<a href="?index_page=<?php echo $i ?>"><?php echo $i ?></a>
-			<?php } ?>
-
-		</div>
-
+	
+<body> 
+<?php require '../menu.php'; ?>
+<div class="top">
+	<div class = "search">
+		<form class = "form_search">
+			Tìm kiếm
+			<input type="search" name="search" value = "<?php echo $content_search ?>">
+			<button>
+				<img src="../style/style_image/icon_search.png" width="50px">
+			</button>
+		</form>
 	</div>
 
+	<div class = "login">
+		<a class = "login" href="https://google.com">Đăng nhập</a>
+	</div> 
 </div>
 
+<div class = "bot">
+	<div class = "header">
+		<h1 class = "header" >KHÁCH HÀNG</h1>
+	</div>
+	<br>
+	<?php require '../validate.php' ?>
+	<table class = "table">
+		<tr>
+			<th>Mã</th>
+			<th>Tên khách hàng</th>
+			<th>Lần cuối mua hàng</th>
+			<th>Số tiền bỏ ra</th>
+			<th>Xem chi tiết</th>
+		</tr>
+		<?php foreach ($query_sql_select_customers as $each_customer) { ?>
+		<tr>
+			<td><?php echo $each_customer['id'] ?></td>
+			<td><?php echo $each_customer['name'] ?></td>
+			<td><?php echo $each_customer['last_time'] ?></td>
+			<td><?php echo $each_customer['money'] ?></td>
+			<td>
+				<a href="detail_customer.php?id=<?php echo $each_customer['id'] ?>">Xem</a>
+			</td>
+		</tr>
+		<?php } ?>
+	</table>
+	<?php for ( $i = 1; $i <= $pages; $i++) {  ?>
+		<a href="?index_page=<?php echo $i ?>"><?php echo $i ?></a>
+	<?php } ?>
+
+</div>
 
 </body>
 </html>
